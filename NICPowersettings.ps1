09987b0073752668e907d8a1ead73b79ec71d039
ssingh49 <51349977+ssingh49@users.noreply.github.com>
2019-11-16 11:40:50 +0530

    NicPowersettings.ps1
    script to uncheck powersaving option for NIC, Wireless and Docking station NIC

---
+++ NICPowersettings.ps1
@@ -0,0 +1,50 @@
+#====================================================================================
+#
+#   Version 1.0.0.0
+#   Date : 24-Jun-2019, Sukhvinder
+#
+#
+#====================================================================================
+
+
+[CmdletBinding(SupportsShouldProcess=$True)]
+param( )
+
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope Script).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+Import-Module (Join-Path (Get-ScriptDirectory) "Common.psm1")
+Initialize-Log $Script:PSBoundParameters
+
+$NICs = Get-WmiObject Win32_NetworkAdapter -filter "AdapterTypeID = '0' AND PhysicalAdapter = 'true' AND NOT Description LIKE '%virtual%' AND NOT Description LIKE '%Bluetooth%'"
+$DockState = (Get-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\IDConfigDB\CurrentDockInfo").DockingState
+ if ($DockState -eq "1")
+{
+ #Write-Host ("Machine is Docked")
+Write-Log "Machine is connected to docking station"
+}
+ else
+{
+Write-Log "Machine is not connected to docking station"
+}
+Write-Log "Power management settings for following devices will be disabled"
+Foreach ($NIC in $NICs)
+{
+    $powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi | where {$_.InstanceName -match [regex]::Escape($nic.PNPDeviceID)}
+	
+	Write-Log $nic.Description
+	#Write-Log Device PNPId: $nic.PNPDeviceID
+	#Write-Log Device Current Power Settings: $powerMgmt.Enable
+	#Write-Host ($nic.PNPDeviceID) 
+	#Write-Host ($powerMgmt.Enable)
+	#Write-Host ($nic.Description) 
+
+  If ($powerMgmt.Enable -eq $True)
+    {
+         $powerMgmt.Enable = $False
+         $powerMgmt.psbase.Put()
+    }
+}
